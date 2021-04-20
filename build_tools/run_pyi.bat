29342922a099447e3f00527b899fc8c57ccb1e38
Chris Satterlee <csatt1@gmail.com>
2021-04-20 11:26:34 -0700

    Add semicolon and dot in run_pyi.bat
    Added missing target directory in the last commit of run_pyi.bat

--- build_tools/run_pyi.bat
+++ build_tools/run_pyi.bat
@@ -2,7 +2,7 @@
 pyinstaller --windowed ^
             --noconfirm ^
             --icon="%GITHUB%\coffeegrindsize\build_tools\coffeegrindsize.ico" ^
-            --add-data="%GITHUB%\coffeegrindsize\build_tools\coffeegrindsize.ico" ^
+            --add-data="%GITHUB%\coffeegrindsize\build_tools\coffeegrindsize.ico;." ^
             --name "coffeegrindsize" ^
             --hidden-import cmath ^
             %GITHUB%\coffeegrindsize\coffeegrindsize.py
